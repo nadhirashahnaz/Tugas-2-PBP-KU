544448864589cf97fb5b8fd34fb853a5397c020c
nadhirashahnaz25 <nadhirashahnaz25@gmail.com>
2022-09-14 21:13:05 +0700

    Berhasil Membuat Tugas 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
+
 
 # TODO: Create your views here.
+def show_catalog(request):
+    data = CatalogItem.objects.all()
+    context = {
+        'list_barang': data,
+        'nama': 'Nadhira Shahnaz Zain',
+        'npm' : '2106701186'
+    }
+    return render(request, "katalog.html", context)
